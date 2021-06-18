a82603ab05c441522f988b9e7a3738070dedff8a
Arnel <arnelarnaldo@gmail.com>
2021-06-18 17:13:54 -0600

    End of Sect6.43

---
+++ src/data.js
@@ -0,0 +1,48 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Kevin Durant - NBA All Star 2021',
+      price: 63,
+      image:
+        'https://arnoldhayescom-my.sharepoint.com/:i:/g/personal/arnelarnaldo_arnold-hayes_com/EVyIjYhRWbhCipHiJINmvZYBp-bWMyuLD555HhEctdZfPw?e=c0y3FK',
+    },
+    {
+      id: '2',
+      name: 'Giannis Antetokounmpo - Bobblehead',
+      price: 56,
+      image:
+        'https://arnoldhayescom-my.sharepoint.com/:i:/g/personal/arnelarnaldo_arnold-hayes_com/EZZyc5_Gk8ROkFKuXHUqe9MBvd0kV7hewml5MgopCjW93w?e=cvmqWq',
+    },
+    {
+      id: '3',
+      name: 'Zion Williamson - Bobblehead',
+      price: 62,
+      image:
+        'https://arnoldhayescom-my.sharepoint.com/:i:/g/personal/arnelarnaldo_arnold-hayes_com/EaeWBqfxWvBBmLobotD2ncUBKUyBGHF3gMcv5uFa2C6mkA?e=4iLZAd',
+    },
+    {
+      id: '4',
+      name: 'Stephen Curry - 3 Point All Star 2021',
+      price: 63,
+      image:
+        'https://arnoldhayescom-my.sharepoint.com/:i:/g/personal/arnelarnaldo_arnold-hayes_com/EeSxKg3WQsJKq9vjEpqzad4BTDPMIAH2KOp8y2BybM9p-Q?e=MYd3g9',
+    },
+    {
+      id: '5',
+      name: 'Tim Duncan - The Big Fundamental',
+      price: 57,
+      image:
+        'https://arnoldhayescom-my.sharepoint.com/:i:/g/personal/arnelarnaldo_arnold-hayes_com/ESxy57loUmhDraYAVuHYoVcBQ7Pr3XcZgRiFO4z4cOSTKg?e=iNJ8i4',
+    },
+    {
+      id: '6',
+      name: 'Luka Doncic - Bobblehead',
+      price: 57,
+      image:
+        'https://arnoldhayescom-my.sharepoint.com/:i:/g/personal/arnelarnaldo_arnold-hayes_com/EaeWBqfxWvBBmLobotD2ncUBKUyBGHF3gMcv5uFa2C6mkA?e=Eacs8u',
+    },
+  ],
+}
+
+export default data
